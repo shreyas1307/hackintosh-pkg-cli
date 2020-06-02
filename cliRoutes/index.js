48d94e16c1c596c36e4d4bd0c6d3ee197d9a75bf
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-06-02 22:52:13 +0530

    Written base logic for making API requests for each of the package

--- cliRoutes/index.js
+++ cliRoutes/index.js
@@ -1,3 +1,17 @@
-// import inquirer from 'inquirer'
+const inquirer = require("inquirer");
+const axios = require("axios");
+const prompt = inquirer.createPromptModule();
 
-// const prompt = inquirer.createPromptModule()
+module.exports = class HackintoshPkgInstall {
+    install(answers) {
+        const { packages } = answers;
+        packages.map((answer) => {
+            console.log(answer);
+            //     let api = `https://api.github.com/repos/${answer}/releases`;
+            //     axios
+            //         .get(api)
+            //         .then((x) => console.log(x.data[0].assets[2]))
+            //         .catch((x) => console.log(x));
+        });
+    }
+};
