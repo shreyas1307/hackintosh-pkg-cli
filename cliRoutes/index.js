7ac7a6d2adead5b45896993e577b4af3c4426025
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-06-11 22:04:54 +0530

    Finished download logic for Hackintosh Packages

--- cliRoutes/index.js
+++ cliRoutes/index.js
@@ -1,17 +1,43 @@
 const inquirer = require("inquirer");
-const axios = require("axios");
+const fs = require('fs');
+// const path = require('path')
+const https = require('https')
 const prompt = inquirer.createPromptModule();
+
+const { get } = require('../services');
+const { default: Axios } = require("axios");
+const wget = require('wget-improved')
 
 module.exports = class HackintoshPkgInstall {
     install(answers) {
         const { packages } = answers;
-        packages.map((answer) => {
-            console.log(answer);
-            //     let api = `https://api.github.com/repos/${answer}/releases`;
-            //     axios
-            //         .get(api)
-            //         .then((x) => console.log(x.data[0].assets[2]))
-            //         .catch((x) => console.log(x));
+        fs.mkdirSync('./downloads', { recursive: true });
+        packages.forEach((answer) => {
+            const [user, repo] = answer.split('/')
+            const url = `https://api.github.com/repos/${user}/${repo}/releases/latest`;
+
+            Axios.get(url)
+                .then((response) => {
+                    let filteredUrl = response.data.assets.map(x => ({ name: x.name, url: x.browser_download_url }))
+
+                    filteredUrl.forEach(fileURL => {
+
+                        const file = fs.createWriteStream(`./downloads/${fileURL.name}`)
+
+                        file.on('finish', () => file.close())
+                        https.get(fileURL.url, (data) => {
+                            https.get(data.headers.location, (xdata) => {
+                                xdata.pipe(file)
+                            })
+                        })
+
+                    })
+
+                })
+                .catch((x) => {
+                    console.log("Sorry, Looks like an Error has occurred. Please check your internet connection!")
+                });
         });
     }
 };
+
