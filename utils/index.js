20f81cb136dfe74378fae183a9c5940a2b37d0cd
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-06-13 19:19:17 +0530

    changed, refactored up utility file

--- utils/index.js
+++ utils/index.js
@@ -1,23 +1,16 @@
 module.exports.optionsMaker = (listItem = []) => {
-    let optionsArray = []
+
     let config = {
         type: "list",
-        name: "version",
+        name: `${listItem.package}/version`,
         message:
-            `Select version of ${listItem.name}  you would like to download: \n`,
+            `Select version of ${listItem.package} you would like to download: \n`,
         pageSize: 20,
         choices: [],
     }
     listItem.version.forEach(versionItem => {
         config.choices.push(versionItem.release_version)
-        // options.choices.push(new inquirer.Separator(listItem.package))
-        // listItem.version.forEach(versionItem => {
-        //     options.choices.push(`${listItem.package}@${versionItem.release_version}`)
-        // })
     })
 
-    optionsArray.push(config)
-
-
-    return { options: optionsArray, package: listItem.name }
+    return { options: config, package: listItem.package }
 }
